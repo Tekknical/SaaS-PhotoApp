30afc4c64eafa390c9c05d4e378cdab3519ec004
Tekknical <105798989+Tekknical@users.noreply.github.com>
2023-04-18 15:23:49 -0400

    Home+Help updates

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,11 +4,15 @@ export const Home = () => {
   return (
     <>
       <h1>Home</h1>
+      <p></p>
       <p> Welcome to the React Photo Editor!</p>
+      <p></p>
+      <p>Click the App button to view full screen and edit the beautiful image</p>
       <p>
         Please check out my app and send any feedback to
         bryaldridge@mailinator.com
       </p>
+      <p></p>
     </>
   );
 };
