90bb44678ee83dd9b9c6b440badd888fa06675a1
Tekknical <105798989+Tekknical@users.noreply.github.com>
2023-04-18 22:00:19 -0400

    formatting

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,14 +3,17 @@ import React from "react";
 export const Home = () => {
   return (
     <>
-      <h1>Home</h1>
+      <h1>  Home  </h1>
       <p></p>
-      <p> Welcome to the React Photo Editor!</p>
+      <p>  Welcome to the React Photo Editor!
+        
+      </p>
       <p></p>
-      <p>Click the App button to view full screen and edit the beautiful image</p>
+      <p>       Click the App button to view full screen and edit the beautiful image
+
+      </p>
       <p>
-        Please check out my app and send any feedback to
-        bryaldridge@mailinator.com
+                Please send any feedback to: bryaldridge@mailinator.com
       </p>
       <p></p>
     </>
